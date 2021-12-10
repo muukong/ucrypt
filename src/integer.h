18c3b51064633de252b5a57497566d04cd20d3b4
Urs Mueller <muukong@protonmail.com>
2021-12-10 20:31:06 +0100

    Readd RND error code definition.

--- src/integer.h
+++ src/integer.h
@@ -46,6 +46,7 @@ typedef struct
 #define UC_OK               1       // Indicates that operation was successful
 #define UC_INPUT_ERR        -1      // Indicates that operation was not successful
 #define UC_MEM_ERR          -2      // Insufficient memory
+#define UC_RNG_ERR          -3      // Random number generation error
 
 #define UC_TRUE     1
 #define UC_FALSE    0
