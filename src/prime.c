74d3416f728d63da777ff2f587695c6c5b3689b2
Urs Mueller <muukong@protonmail.com>
2021-12-11 12:21:11 +0100

    Implemented Miller-Rabin primality test.

--- src/prime.c
+++ src/prime.c
@@ -1,5 +1,6 @@
 #include "integer.h"
 #include "prime.h"
+#include "rand.h"
 
 /*
  * Small list of prime numbers for trial division. This list was chosen for two reasons:
@@ -46,6 +47,79 @@ int uc_is_prime_trial_division(uc_int *x, int *is_prime)
 
 cleanup:
     uc_free(&q);
+
+    return res;
+}
+
+int uc_is_prime_miller_rabin(uc_int *n, int *is_prime, int t)
+{
+    int i, j, s, res;
+    uc_int a, r, tmp, n1;
+
+    res = UC_OK;
+
+    if ((res = uc_init_multi(&a, &r, &tmp, &n1, 0, 0)) != UC_OK )
+        return res;
+
+    /* n1 = n - 1 */
+    if ( (res = uc_sub_d(&n1, n, 1)) != UC_OK )
+        goto cleanup;
+
+    /*
+     * Compute r and s such that n - 1 = 2^s * r
+     */
+    uc_copy(&r, n);
+    uc_sub_d(&r, &r, 1);
+    s = 0;
+    while ( uc_is_even(&r) )
+    {
+        uc_div_2(&r, &r);
+        ++s;
+    }
+
+    /* Run t tests */
+    for ( i = 0; i < t; ++i )
+    {
+        /* Sample a in [2,n-1) */
+        if ( (res = uc_set_i(&tmp, 2)) != UC_OK ||
+             (res = uc_rand_int_range(&a, &tmp, &n1)) != UC_OK )
+        {
+            goto cleanup;
+        }
+
+        /* a = a^r mod n */
+        if ( (res = uc_exp_mod(&a, &a, &r, n)) != UC_OK )
+            goto cleanup;
+
+        if ( uc_is_one(&a) || uc_eq(&a, &n1) )
+            continue;
+
+        j = 1;
+        while ( j <= s - 1 && !uc_eq(&a, &n1) )
+        {
+            if ( (res = uc_mul_mod(&a, &a, &a, n)) != UC_OK )
+                goto cleanup;
+
+            if ( uc_is_one(&a) )
+            {
+                *is_prime = UC_FALSE;
+                goto cleanup;
+            }
+
+            ++j;
+        }
+
+        if ( !uc_eq(&a, &n1) )
+        {
+            *is_prime = UC_FALSE;
+            goto cleanup;
+        }
+    }
+
+    *is_prime = UC_TRUE;
+
+cleanup:
+    uc_free_multi(&a, &r, &tmp, &n1, 0, 0);
 
     return res;
 }
--- src/prime.h
+++ src/prime.h
@@ -4,5 +4,6 @@
 int uc_is_prime(uc_int *x, int *is_prime);
 
 int uc_is_prime_trial_division(uc_int *x, int *is_prime);
+int uc_is_prime_miller_rabin(uc_int *n, int *is_prime, int t);
 
 #endif //UCRYPT_PRIME_H
