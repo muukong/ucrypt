e5017081a90561e1f5ec22302eb383ff05e892fb
Urs Mueller <muukong@protonmail.com>
2021-12-28 16:54:44 +0100

    Do not override existing test data files.

--- tests/datagen/datagen.sh
+++ tests/datagen/datagen.sh
@@ -4,5 +4,13 @@ WORK_DIR=$(dirname "$0")
 DATADIR="$WORK_DIR"/../data
 
 mkdir -p "$DATADIR"
-python3 "$WORK_DIR"/muldata.py > "$DATADIR"/mul.csv
-python3 "$WORK_DIR"/divdata.py > "$DATADIR"/div.csv
+
+MUL_DATA_FILE="$DATADIR"/mul.csv
+if [ ! -f "$MUL_DATA_FILE" ]; then
+  python3 "$WORK_DIR"/muldata.py > "$DATADIR"/mul.csv
+fi
+
+DIV_DATA_FILE="$DATADIR"/div.csv
+if [ ! -f "$DIV_DATA_FILE" ]; then
+  python3 "$WORK_DIR"/divdata.py > "$DATADIR"/div.csv
+fi
