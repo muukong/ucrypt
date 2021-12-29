b58e9a760a462ab663ef0c3c3bf5a2c10e59de0d
Urs Mueller <muukong@protonmail.com>
2021-12-29 17:55:13 +0100

    Remove old code.

--- tests/include/uctest/testcase.h
+++ tests/include/uctest/testcase.h
@@ -18,30 +18,5 @@ int uc_testcase_free(uc_csv_testcase *t);
 int uc_testcase_next(uc_csv_testcase *t);
 int uc_testcase_col(uc_csv_testcase*t, int col, char *data, size_t data_len);
 
-/*
-typedef struct
-{
-    FILE *fhandle;
-    char *line;
-    size_t line_size;
-} uc_test;
-
-typedef struct
-{
-    char **data;
-    int n;
-} uc_testcase;
-
-int uc_test_init(uc_test *t, const char *fpath);
-int uc_test_parse_line(uc_test *t, const char *delim, uc_testcase *tc);
-int uc_test_next(uc_test *t);
-
-
-
-int uc_testcase_init(uc_testcase *tc, int n);
-
-int uc_testcase_free(uc_testcase *tc);
- */
-
 #endif //UCRYPT_TESTCASE_H
 
