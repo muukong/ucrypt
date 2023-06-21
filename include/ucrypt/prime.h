2e1b805eb22881b62a159f0420b347796c66c3ff
muukong <muukong@protonmail.com>
2023-06-21 16:22:26 +0200

    Modify function signature to prevent compile-time error.

--- include/ucrypt/prime.h
+++ include/ucrypt/prime.h
@@ -1,7 +1,7 @@
 #ifndef UCRYPT_PRIME_H
 #define UCRYPT_PRIME_H
 
-int uc_is_prime(uc_int *x, int *is_prime);
+int uc_is_prime(uc_int *x, int *is_prime, int safe);
 
 int uc_is_prime_trial_division(uc_int *x, int *is_prime);
 int uc_is_prime_miller_rabin(uc_int *n, int *is_prime, int t);
