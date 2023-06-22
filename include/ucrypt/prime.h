090ccf18ccd0d3e5447d3154f2804fc0066c600c
muukong <muukong@protonmail.com>
2023-06-22 11:29:51 +0200

    Typo fix.

--- include/ucrypt/prime.h
+++ include/ucrypt/prime.h
@@ -5,6 +5,6 @@ int uc_is_prime(uc_int *x, int *is_prime, int safe);
 
 int uc_is_prime_trial_division(uc_int *x, int *is_prime);
 int uc_is_prime_miller_rabin(uc_int *n, int *is_prime, int t);
-int gen_rand_prime(uc_int *x, uc_int *a, uc_int *b);
+int uc_gen_rand_prime(uc_int *x, uc_int *a, uc_int *b);
 
 #endif //UCRYPT_PRIME_H
